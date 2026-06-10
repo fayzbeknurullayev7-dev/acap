4215da873b0204f3ae7e378dc17635ff61955708
fayzbeknurullayev7-dev <fayzbeknurullayev7@gmail.com>
2026-06-10 20:38:25 +0500

    Update MainActivity to use Flutter v2 embedding with proper plugin registration

--- acap_app/android/app/src/main/kotlin/com/acap/app/MainActivity.kt
+++ acap_app/android/app/src/main/kotlin/com/acap/app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.acap.app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
