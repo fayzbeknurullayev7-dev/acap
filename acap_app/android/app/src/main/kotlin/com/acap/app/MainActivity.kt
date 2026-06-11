dff89de8d87f378980177563958c234a9b0e4f3c
root <root@localhost.localdomain>
2026-06-11 11:25:45 +0500

    Fix: Regenerate Android folder with flutter create

--- acap_app/android/app/src/main/kotlin/com/acap/app/MainActivity.kt
+++ acap_app/android/app/src/main/kotlin/com/acap/app/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.acap.app
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.android.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-        super.configureFlutterEngine(flutterEngine)
-    }
-}
+class MainActivity : FlutterActivity()
